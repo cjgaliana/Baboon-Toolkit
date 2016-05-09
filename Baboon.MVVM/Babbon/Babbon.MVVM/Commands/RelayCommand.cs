6e4bb15490df8a5bd50ec1a094fc70081d5377b6
Camilo Galiana <cjgaliana@gmail.com>
2016-05-09 15:52:07 +0100

    Mvvmlight implementation of RelayCommand

--- Baboon.MVVM/Babbon/Babbon.MVVM/Commands/RelayCommand.cs
+++ Baboon.MVVM/Babbon/Babbon.MVVM/Commands/RelayCommand.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿// <copyright file="RelayCommand.cs" company="GalaSoft Laurent Bugnion">
+// Copyright © GalaSoft Laurent Bugnion 2009-2011
+// </copyright>
+// ****************************************************************************
+// <author>Laurent Bugnion</author>
+// <email>laurent@galasoft.ch</email>
+// <date>22.4.2009</date>
+// <project>GalaSoft.MvvmLight</project>
+// <web>http://www.galasoft.ch</web>
+// <license>
+// See license.txt in this project or http://www.galasoft.ch/license_MIT.txt
+// </license>
+// <LastBaseLevel>BL0010</LastBaseLevel>
+// ****************************************************************************
+// <credits>This class was developed by Josh Smith (http://joshsmithonwpf.wordpress.com) and
+// slightly modified with his permission.</credits>
+// ****************************************************************************
+
+using System;
 using System.Windows.Input;
 
 namespace Babbon.MVVM.Commands
@@ -8,10 +26,23 @@ namespace Babbon.MVVM.Commands
         private readonly Func<bool> _canExecute;
         private readonly Action _execute;
 
-        public RelayCommand(Action execute) : this(execute, null)
+        /// <summary>
+        ///     Initializes a new instance of the RelayCommand class that
+        ///     can always execute.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        /// <exception cref="ArgumentNullException">If the execute argument is null.</exception>
+        public RelayCommand(Action execute)
+            : this(execute, null)
         {
         }
 
+        /// <summary>
+        ///     Initializes a new instance of the RelayCommand class.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        /// <param name="canExecute">The execution status logic.</param>
+        /// <exception cref="ArgumentNullException">If the execute argument is null.</exception>
         public RelayCommand(Action execute, Func<bool> canExecute)
         {
             if (execute == null)
@@ -20,41 +51,125 @@ namespace Babbon.MVVM.Commands
             }
 
             _execute = execute;
-
-            if (canExecute != null)
-            {
-                _canExecute = canExecute;
-            }
+            _canExecute = canExecute;
         }
 
+        /// <summary>
+        ///     Occurs when changes occur that affect whether the command should execute.
+        /// </summary>
         public event EventHandler CanExecuteChanged;
 
         /// <summary>
+        ///     Defines the method that determines whether the command can execute in its current state.
         /// </summary>
-        /// <param name="parameter">This parameter will be always ignored</param>
-        /// <returns></returns>
+        /// <param name="parameter">This parameter will always be ignored.</param>
+        /// <returns>true if this command can be executed; otherwise, false.</returns>
         public bool CanExecute(object parameter)
         {
-            if (_canExecute == null)
-            {
-                return true;
-            }
-
-            return _canExecute.Invoke();
+            return _canExecute == null ? true : _canExecute();
         }
 
+        /// <summary>
+        ///     Defines the method to be called when the command is invoked.
+        /// </summary>
+        /// <param name="parameter">This parameter will always be ignored.</param>
         public void Execute(object parameter)
         {
             if (CanExecute(parameter))
             {
-                _execute?.Invoke();
+                _execute();
             }
         }
 
+        /// <summary>
+        ///     Raises the <see cref="CanExecuteChanged" /> event.
+        /// </summary>
         public void RaiseCanExecuteChanged()
         {
             var handler = CanExecuteChanged;
-            handler?.Invoke(this, EventArgs.Empty);
+            if (handler != null)
+            {
+                handler(this, EventArgs.Empty);
+            }
+        }
+    }
+
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Func<T, bool> _canExecute;
+        private readonly Action<T> _execute;
+
+        /// <summary>
+        ///     Initializes a new instance of the RelayCommand class that
+        ///     can always execute.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        /// <exception cref="ArgumentNullException">If the execute argument is null.</exception>
+        public RelayCommand(Action<T> execute)
+            : this(execute, null)
+        {
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the RelayCommand class.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        /// <param name="canExecute">The execution status logic.</param>
+        /// <exception cref="ArgumentNullException">If the execute argument is null.</exception>
+        public RelayCommand(Action<T> execute, Func<T, bool> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException(nameof(execute));
+            }
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        /// <summary>
+        ///     Occurs when changes occur that affect whether the command should execute.
+        /// </summary>
+        public event EventHandler CanExecuteChanged;
+
+        /// <summary>
+        ///     Defines the method that determines whether the command can execute in its current state.
+        /// </summary>
+        /// <param name="parameter">
+        ///     Data used by the command. If the command does not require data
+        ///     to be passed, this object can be set to a null reference
+        /// </param>
+        /// <returns>true if this command can be executed; otherwise, false.</returns>
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute((T) parameter);
+        }
+
+        /// <summary>
+        ///     Defines the method to be called when the command is invoked.
+        /// </summary>
+        /// <param name="parameter">
+        ///     Data used by the command. If the command does not require data
+        ///     to be passed, this object can be set to a null reference
+        /// </param>
+        public void Execute(object parameter)
+        {
+            if (CanExecute(parameter))
+            {
+                _execute((T) parameter);
+            }
+        }
+
+        /// <summary>
+        ///     Raises the <see cref="CanExecuteChanged" /> event.
+        /// </summary>
+        public void RaiseCanExecuteChanged()
+        {
+            var handler = CanExecuteChanged;
+            if (handler != null)
+            {
+                handler(this, EventArgs.Empty);
+            }
         }
     }
 }
