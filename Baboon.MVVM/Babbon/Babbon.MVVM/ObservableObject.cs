9ccb9b2e1ed82534ec9bf21f62e9ef27cc47afff
Camilo Galiana <camilo.galiana-bevia@rbs.co.uk>
2017-06-16 14:59:16 +0100

    Updated to Netstandard

--- Baboon.MVVM/Babbon/Babbon.MVVM/ObservableObject.cs
+++ Baboon.MVVM/Babbon/Babbon.MVVM/ObservableObject.cs
@@ -1,24 +1,27 @@
-﻿using System.ComponentModel;
-using System.Runtime.CompilerServices;
-
-namespace Babbon.MVVM
+﻿namespace Babbon.MVVM
 {
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
     public class ObservableObject : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
-            var handler = PropertyChanged;
+            var handler = this.PropertyChanged;
             handler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
         protected virtual bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
         {
-            if (Equals(storage, value)) return false;
+            if (Equals(storage, value))
+            {
+                return false;
+            }
 
             storage = value;
-            OnPropertyChanged(propertyName);
+            this.OnPropertyChanged(propertyName);
 
             return true;
         }
